c0ec4726cb24370651fcf42a5ceb9e15edad114c
kquinones107 <kquinones@unal.edu.co>
2026-03-20 21:56:08 -0500

    corregido nombre mal en el import

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { updateProfile } = require('../controllers/authController');
-const requireClerkAuth = require("../middlewares/requirerequireClerkAuth");
+const requireClerkAuth = require("../middlewares/requireClerkAuth");
 
 router.put('/profile', requireClerkAuth, updateProfile);
 
